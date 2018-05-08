fa1d582213cb05a2e5dad0e5bae206ff5cf93d0d
MarkJGx <38701409+MarkJGx@users.noreply.github.com>
2018-05-08 17:57:32 +0300

    added text to squares

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import argparse
 
 #parser = argparse.ArgumentParser()
 #parser.add_argument('commandgoeshere', type=str)
-
+# Tikko Word Clock Matrix
 MATRIX_ROWS = 12
 MATRIX_COLUMNS = 12
 SPACING = 1.25;
@@ -18,10 +18,20 @@ squares = dict()
 
 for row in range(MATRIX_ROWS):
     for column in range(MATRIX_COLUMNS):
-        squares[row + MATRIX_COLUMNS * column] = square = plt.Rectangle((SPACING * row, SPACING * column), 1, 1, fc='g')
+        square_size = 1
+        index = row + MATRIX_COLUMNS * column
+        x,y = SPACING * row, SPACING * column
+        squares[row + MATRIX_COLUMNS * column] = square = plt.Rectangle((x,y), square_size, square_size, fc='g')
         plt.gca().add_patch(square)
+        plt.text(0.15+x, 0.125+y, '1', fontsize=16)
+
+fig = plt.gcf()
+fig.canvas.set_window_title('Tikko Word Clock Emulator')
+
 
 plt.axis('scaled')
+
+
 plt.show()
 
 
